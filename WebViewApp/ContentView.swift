8ccaa0fe87e7a561b40a85c93d01a60f41cc49ec
boazb54 <59447244+boazb54@users.noreply.github.com>
2026-01-19 19:13:09 +0200

    Update ContentView.swift

--- WebViewApp/ContentView.swift
+++ WebViewApp/ContentView.swift
@@ -1,1 +1,8 @@
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://vibelist-plus-git-test-boazs-projects-89c69ca6.vercel.app")!)
+            .ignoresSafeArea()
+    }
+}

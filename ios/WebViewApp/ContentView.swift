ef022e289da1fe0939219469796a443f8bf34194
boazb54 <59447244+boazb54@users.noreply.github.com>
2026-01-19 21:40:03 +0200

    Delete ios/WebViewApp directory

--- ios/WebViewApp/ContentView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        WebView(url: URL(string: "https://vibelist-plus-git-test-boazs-projects-89c69ca6.vercel.app/")!)
-            .ignoresSafeArea()
-    }
-}
